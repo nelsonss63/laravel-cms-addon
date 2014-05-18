320a4738891e4a89c5cd83541de78cda400fd588
Johnny Cedergren <johnny.cedergren@svmc.se>
2014-05-18 15:04:18 +0200

    Can edit? show edit (pen) icon

--- src/views/templates/page.blade.php
+++ src/views/templates/page.blade.php
@@ -3,6 +3,13 @@
 
 @section($sectionName)
 
+    {{-- On Page Edit Link for Admin And Editors --}}
+    @if(\Cms\Models\User::check() AND \Cms\Models\User::getUser()->admin AND (!empty($allow_edit_page) AND $allow_edit_page === TRUE))
+    <div style="float: right; margin-top: 3px;">
+        <a href="{{ route('editPage', array($page->id)) }}"><img src="/packages/cednet/laravel-cms-addon/css/images/pen-icon.png" /></a>
+    </div>
+    @endif
+
    <h1>{{ $page->content->title }}</h1>
    <p>{{ $page->content->body }}</p>
 
